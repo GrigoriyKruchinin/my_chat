30ac8f9f5e166a3f044f375447af3be97321a934
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-10-17 15:13:43 +0200

    start project

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    database_url: str = ""
+
+    secret_key: str
+
+    class ConfigDict:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ initial_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo -e "\n\n\nНачало начальной настройки...\n\n\n"
+
+echo -e "\n\n\nСборка и запуск контейнеров...\n\n\n"
+docker-compose up --build -d db app
+
+echo -e "\n\n\nПрименение миграций...\n\n\n"
+docker-compose run app alembic upgrade head
+
+echo -e "\n\n\nНачальная настройка завершена. Приложение запущено.\n\n\n"
+docker-compose up app
+
+echo -e "\n\n\nПриложение остановлено.\n\n\n"
